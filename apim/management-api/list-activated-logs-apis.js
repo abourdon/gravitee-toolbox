51c221cf70fcba1d3f2cf63e8b1922b3f37b3f4e
Alexandre Carbenay <acarbenay@partner.auchan.com>
2019-08-28 09:58:44 +0200

    feat(management-api): Disable detailed logs on demand

--- apim/management-api/list-activated-logs-apis.js
+++ apim/management-api/list-activated-logs-apis.js
@@ -1,7 +1,10 @@
 const ManagementApiScript = require('./lib/management-api-script');
-const { tap, filter, flatMap, map } = require('rxjs/operators');
+const Rx = require('rxjs')
+const { filter, flatMap, map } = require('rxjs/operators');
 const util = require('util');
 
+const API_STATE_STARTED = 'started';
+const LOGGING_MODE_DISABLED = 'NONE';
 const TIMESTAMP_CONDITION_PREFIX = '#request.timestamp <= ';
 
 const now = (new Date).getTime();
@@ -24,6 +27,11 @@ class ListActivatedLogsApis extends ManagementApiScript {
                     describe: "Filter APIs against context-path (insensitive regex)",
                     type: 'string'
                 },
+                'disable-logs': {
+                    describe: "Flag used to disable detailed logs for found APIs",
+                    type: 'boolean',
+                    default: false
+                }
             }
         );
     }
@@ -38,15 +46,23 @@ class ListActivatedLogsApis extends ManagementApiScript {
                 })),
                 map(api => api.details),
                 filter(api => api.proxy.logging != undefined &&
-                              api.proxy.logging.mode != 'NONE' &&
-                              this.isLoggingConditionEnabled(api.proxy.logging.condition)),
+                              api.proxy.logging.mode != LOGGING_MODE_DISABLED &&
+                              this.isLoggingConditionActive(api.proxy.logging.condition)),
+                flatMap(api => managementApi.getApi(api.id)),
+                flatMap(api => managementApi.getApiState(api.id).pipe(
+                    map(state => {
+                        api.is_synchronized = state.is_synchronized;
+                        return api;
+                    })
+                )),
+                flatMap(api => this.disableLogs(managementApi, api)),
             )
-            .subscribe(this.defaultSubscriber(
-                api => this.displayRaw(util.format('%s (%s): %s - %s', api.name, api.id, api.proxy.logging.mode, api.proxy.logging.condition))
-            ));
+            .subscribe(this.defaultSubscriber(api => {
+                this.displayRaw(util.format('%s (%s) - Logging condition: %s', api.name, api.id, api.proxy.logging.condition));
+            }));
     }
 
-    isLoggingConditionEnabled(condition) {
+    isLoggingConditionActive(condition) {
         return condition == undefined || !this.isLoggingConditionATimestamp(condition) || this.isTimestampConditionEnabled(condition);
     }
 
@@ -57,6 +73,24 @@ class ListActivatedLogsApis extends ManagementApiScript {
     isTimestampConditionEnabled(condition) {
         var timestamp = condition.substring(TIMESTAMP_CONDITION_PREFIX.length, condition.length - 1);
         return Number(timestamp) > now;
+    }
+
+    disableLogs(managementApi, api) {
+        if (!this.argv['disable-logs']) {
+            return Rx.of(api);
+        }
+        if (!api.is_synchronized) {
+            this.displayRaw(util.format('Logs for API %s (%s) have not been disabled: last changes have not been deployed', api.name, api.id));
+            return Rx.of(api);
+        }
+        delete api.is_synchronized;
+
+        api.proxy.logging.mode = LOGGING_MODE_DISABLED;
+        return managementApi
+            .update(api, api.id)
+            .pipe(
+                flatMap(updatedApi => managementApi.deploy(updatedApi.id))
+            );
     }
 
 }
