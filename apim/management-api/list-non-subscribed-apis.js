01fcf869aa0f14d5cecc19e2a34cc0a998a5a221
Alexandre Carbenay <acarbenay@partner.auchan.com>
2019-09-19 16:52:31 +0200

    feat(management-api): Define script to list all APIs with no active subscription

---
+++ apim/management-api/list-non-subscribed-apis.js
@@ -0,0 +1,55 @@
+const ManagementApiScript = require('./lib/management-api-script');
+const { filter, flatMap, map } = require('rxjs/operators');
+const util = require('util');
+
+const NO_DELAY_PERIOD = 0;
+
+/**
+ * List all APIs with no active subscription by displaying their name, context path, owner name and owner email.
+ *
+ * @author Alexandre Carbenay
+ */
+class ListNonSubscribedApis extends ManagementApiScript {
+
+    constructor() {
+        super(
+            'list-non-subscribed-apis',
+            {
+                'filter-by-name': {
+                    describe: "Filter APIs against their name (insensitive regex)",
+                    type: 'string'
+                },
+                'filter-by-context-path': {
+                    describe: "Filter APIs against context-path (insensitive regex)",
+                    type: 'string'
+                }
+            }
+        );
+    }
+
+    definition(managementApi) {
+        managementApi
+            .login(this.argv['username'], this.argv['password'])
+            .pipe(
+                flatMap(_token => managementApi.listApis({
+                            byName: this.argv['filter-by-name'],
+                            byContextPath: this.argv['filter-by-context-path']
+                        }, NO_DELAY_PERIOD)),
+                flatMap(api => managementApi.getApiSubscriptions(api.id).pipe(
+                    filter(subscriptions => subscriptions.data.length == 0),
+                    map(subscriptions => api)
+                ))
+            )
+            .subscribe(this.defaultSubscriber(
+                api => this.displayRaw(util.format('[%s, %s, %s <%s>] %s',
+                    api.id,
+                    api.context_path,
+                    api.owner.displayName,
+                    api.owner.email,
+                    api.name
+                ))
+            ));
+    }
+}
+
+new ListNonSubscribedApis().run();
