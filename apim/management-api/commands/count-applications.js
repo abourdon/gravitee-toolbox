da6be979cf36dca9e4f29d7a26f7563103c929ee
Alexandre Carbenay <acarbenay@partner.auchan.com>
2019-10-23 15:15:50 +0200

    feat(management-api): Add script to count applications

---
+++ apim/management-api/commands/count-applications.js
@@ -0,0 +1,32 @@
+const CliCommand = require('./lib/cli-command');
+const { count, flatMap } = require('rxjs/operators');
+const util = require('util');
+
+const NO_DELAY_PERIOD = 0;
+
+/**
+ * Count all registered applications.
+ *
+ * @author Alexandre Carbenay
+ */
+class CountApplications extends CliCommand {
+
+    constructor() {
+        super(
+            'count-applications'
+        );
+    }
+
+    definition(managementApi) {
+        managementApi
+            .login(this.argv['username'], this.argv['password'])
+            .pipe(
+                flatMap(() => managementApi.listApplications(NO_DELAY_PERIOD)),
+                count()
+            )
+            .subscribe(this.defaultSubscriber(
+                apiCount => this.displayRaw(util.format('There are %s applications in the requested environment', apiCount))
+            ));
+    }
+}
+new CountApplications().run();
